28962de6890488498575d1210f697e40b4d62ab5
Afolabi <linktoyinka@gmail.com>
2023-10-18 18:26:50 +0100

    nav menu closing when click outside the container

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,40 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { BsFilterRight, BsX } from 'react-icons/bs'
-import {Link} from 'react-router-dom'
-import { images } from '../../constants'
-import './Navbar.css'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import React, { useEffect, useState, useRef } from "react";
+import { BsFilterRight, BsX } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { images } from "../../constants";
+import "./Navbar.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleNav = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const navRef = useRef(null);
 
-    useEffect(()=>{
-        Aos.init({duration:1000})
-    },[])
+  const handleNav = () => setNav(!nav);
 
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
 
-    return (
-        <nav className='navbar'>
-            <div className="container">
-                <a className="logo" href='/'>
-                    <img src={images.logo} alt='logo' />
-                </a>
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setNav(false);
+      }
+    };
 
-                <ul className={nav ? 'nav-menu active scale-up-center' : 'nav-menu'}>
-                    <li><Link onClick={() => setNav(false)} to='/'>Home</Link></li>
-                    <li><Link onClick={() => setNav(false)} to='/about'>About</Link></li>
-                    <li><Link onClick={() => setNav(false)} to='/work'>Work</Link></li>
-                    <li><a onClick={() => setNav(false)} href='https:blog.afolabi.me' target='blank'>Blog</a></li>
-                    <li><a onClick={() => setNav(false)} href='#contact'>Contact</a></li>
-                </ul>
-                <div className="hamburger" onClick={handleNav}>
-                    {!nav ? (<BsFilterRight className='icon' />) : (<BsX className='icon' />)}
-                </div>
-            </div>
-        </nav>
-    )
-}
+    document.addEventListener("mousedown", handleClickOutside);
 
-export default Navbar
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <nav className="navbar" ref={navRef}>
+      <div className="container">
+        <a className="logo" href="/">
+          <img src={images.logo} alt="logo" />
+        </a>
+
+        <ul className={nav ? "nav-menu active scale-up-center" : "nav-menu"}>
+          <li><Link onClick={() => setNav(false)} to="/">Home</Link></li>
+          <li><Link onClick={() => setNav(false)} to="/about">About</Link></li>
+          <li><Link onClick={() => setNav(false)} to="/work">Work</Link></li>
+          <li><a onClick={() => setNav(false)} href="https:blog.afolabi.me" target="blank" >Blog</a></li>
+          <li><a onClick={() => setNav(false)} href="#contact">Contact</a></li>
+        </ul>
+        <div className="hamburger" onClick={handleNav}>
+          {!nav ? <BsFilterRight className="icon" /> : <BsX className="icon" />}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
