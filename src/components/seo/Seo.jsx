3dcd43622bb32aceb67d7c66cd88520fd6e94f48
Afolabi <linktoyinka@gmail.com>
2023-10-19 11:37:02 +0100

    SEO updated

--- src/components/seo/Seo.jsx
+++ src/components/seo/Seo.jsx
@@ -6,9 +6,18 @@ const Seo = ({ title, description }) => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
-        <title>{title}</title>
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={images.ogimage} />
+      <title>{title}</title>
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content='https://afolabi.me'/>
+      <meta property="og:title" content={title}/>
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={images.ogimage}/>
+
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content='https://afolabi.me'/>
+      <meta property="twitter:title" content={title}/>
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={images.ogimage}/>
     </Helmet>
   )
 }
