f91d01e59cd5734eafb00f94c1259ac954b05058
Afolabi <linktoyinka@gmail.com>
2023-11-26 16:11:34 +0100

    Update ReactDOM rendering method

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// )
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+// Check if the script is running on the server
+const isServer = typeof window === 'undefined';
+
+// Use ReactDOM.hydrateRoot if running on the client-side
+const renderMethod = isServer ? ReactDOM.createRoot : ReactDOM.createRoot;
+
+// Render the application
+renderMethod(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
